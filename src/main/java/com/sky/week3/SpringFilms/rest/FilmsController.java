b997be4050ada22dc0472fb7d1f1f2bd8f35d110
siamn <siam.nafis@gmail.com>
2023-07-31 14:43:41 +0100

    added query params remove methods

--- src/main/java/com/sky/week3/SpringFilms/rest/FilmsController.java
+++ src/main/java/com/sky/week3/SpringFilms/rest/FilmsController.java
@@ -1,11 +1,11 @@
 package com.sky.week3.SpringFilms.rest;
 
 import com.sky.week3.SpringFilms.domain.Film;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,8 +28,55 @@ public class FilmsController {
     @GetMapping("/get/{id}")
     public Film getById(@PathVariable Integer id) {
         System.out.println("ID: " + id);
-        return films.get(id);
+        Film get = films.get(id);
+        System.out.println(get);
+        return get;
     }
+
+
+    @GetMapping("/getAll")
+    public List<Film> getAll() {
+        System.out.println(films);
+        return films;
+    }
+
+
+    @DeleteMapping("/remove/{id}")
+    public Film remove(@PathVariable Integer id) {
+        return films.remove((int) id);
+
+    }
+
+
+    @DeleteMapping("/remove")
+    public Boolean remove(
+            @PathParam("year") Integer year,
+            @PathParam("title") String title,
+            @PathParam("genre") String genre) {
+
+        System.out.println("Title: "+title);
+        System.out.println("Genre: "+genre);
+        System.out.println("Year:"+ year);
+
+        System.out.println("Before: "+films);
+        int initialSize = films.size();
+//        for (Film film : new ArrayList<>(films)) {
+//            if ((film.getTitle().equals(title)) || ((film.getGenre().equals(genre)))
+//            || (film.getYear().equals(year))) {
+//                films.remove(film);
+//            }
+//        }
+
+        films.removeIf(film -> (film.getTitle().equals(title))
+                || film.getGenre().equals(genre)
+                || film.getYear().equals(year));
+
+
+        System.out.println("After: "+films);
+        return films.size() < initialSize;
+    }
+
+
 
 
 }
