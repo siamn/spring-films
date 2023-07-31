f4d5c550c7f5b25f5edc0634b98d834d9d893a75
siamn <siam.nafis@gmail.com>
2023-07-31 10:19:48 +0100

    init

---
+++ src/test/java/com/sky/week3/SpringFilms/SpringFilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.week3.SpringFilms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
