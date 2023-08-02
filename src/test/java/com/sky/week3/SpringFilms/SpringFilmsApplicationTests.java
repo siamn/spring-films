6dc3b56631487e6bba9977e23b5e259590a7d1ab
siamn <siam.nafis@gmail.com>
2023-08-02 17:03:09 +0100

    added mysql connection

--- src/test/java/com/sky/week3/SpringFilms/SpringFilmsApplicationTests.java
+++ src/test/java/com/sky/week3/SpringFilms/SpringFilmsApplicationTests.java
@@ -2,8 +2,10 @@ package com.sky.week3.SpringFilms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringFilmsApplicationTests {
 
 	@Test
--- src/test/java/com/sky/week3/SpringFilms/rest/FilmsMVCTest.java
+++ src/test/java/com/sky/week3/SpringFilms/rest/FilmsMVCTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // loads the application context
 @AutoConfigureMockMvc // create a MockMVC bean
 @Sql(scripts = {"classpath:film-schema.sql", "classpath:film-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 public class FilmsMVCTest {
 
 
